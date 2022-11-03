fe093b4a5e7191468e48cb516180e8479d28e614
Sri Swathi <45977029+SriSwathi25@users.noreply.github.com>
2022-11-03 23:03:05 +0530

    Added Files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const  sr = ScrollReveal({
+reset: true
+});
+
+/*  About Me Section */
+sr.reveal('.home_title',{delay: 200});
+sr.reveal('.home_img',{delay: 400}); 
+sr.reveal('.about_me_text',{delay: 500}); 
+sr.reveal('.about_social_icons',{delay: 600}); 
+
+/*  Work, Academics Section */
+sr.reveal('.section-title, span i',{delay: 200});
+sr.reveal('.timeline',{delay: 200});
+
+sr.reveal('.connect-form',{delay:100});
+sr.reveal('.skill_container', {delay:100});
+
+
